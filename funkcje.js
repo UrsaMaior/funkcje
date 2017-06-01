1eff308df90e42df8f27350ff99ce71178f524ca
Your Name <you@example.com>
2017-06-01 18:21:27 +0200

    not good

---
+++ funkcje.js
@@ -0,0 +1,17 @@
+var a = 10;
+var h = 15;
+var resultTriangleArea = a*h/2;
+
+
+function getTriangleArea(a, h) {
+    if (getTriangleArea<=0) {
+        console.log("Nieprawidłowe dane")
+}	if (getTriangleArea>0) {
+        console.log(resultTriangleArea)
+}
+
+}
+
+console.log( getTriangleArea(10,6)
+
+var triangle1Area = getTriangleArea(10, 15, 9); 
